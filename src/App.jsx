8e6c0b561895636d1aeb78c44e5a601bf83a6e75
yashjangir04 <jangiryash04@gmail.com>
2025-09-28 16:14:46 +0530

    UI Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Cards from './components/Cards'
 
 const App = () => {
   return (
-    <div className='w-full min-h-screen flex flex-col justify-center items-center'>
+    <div className='w-full min-h-screen flex flex-col justify-center items-center bg-green-300'>
       <h1 className='text-4xl lg:text-5xl absolute top-10 text-center'>Our Models & Features</h1>
       <div className="flex flex-col md:flex-row gap-25 md:gap-10 mt-[200px] md:mt-30">
         <Cards modelName={"Breed Recognition Model"} desc = {"AI-powered breed identification system for accurate and instant results."} link = {"https://sihproject-git-main-yashjangir04s-projects.vercel.app/"} imgNo={1} />
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,11 +7,11 @@ const Cards = ({modelName , desc , link , imgNo}) => {
 
   return (
     <div
-      className="w-[300px] h-[330px] bg-green-300 rounded-3xl transition-all duration-500 relative flex flex-col items-center px-[20px] group hover:translate-y-[-4px] cursor-pointer"
+      className="w-[300px] h-[330px] bg-white rounded-3xl transition-all duration-500 relative flex flex-col items-center px-[20px] group hover:translate-y-[-4px] cursor-pointer"
       onClick={handleClick}
     >
       <div className="image w-[160px] h-[150px] transition-all duration-500 bg-green-700 absolute top-[-40px] rounded-lg group-hover:translate-y-[1px] overflow-hidden">
-        <img src={`/${imgNo}.png`} alt="" />
+        <img src={`./${imgNo}.png`} alt="" />
       </div>
       <h1 className=" mt-[140px] dm-sans tracking-tight text-xl text-green-900">
         {modelName}
@@ -20,7 +20,7 @@ const Cards = ({modelName , desc , link , imgNo}) => {
         <p className="poppins-regular text-center text-black mt-2">
           {desc}
         </p>
-        <button className="mt-[25px] bg-green-500 px-4 py-2 rounded-md group cursor-pointer transition-all duration-300 hover:bg-green-600">
+        <button className="mt-[25px] bg-green-900 px-4 py-2 rounded-md group cursor-pointer transition-all duration-300 hover:bg-green-500 hover:scale-102 text-white">
           Try Now
         </button>
       </div>
